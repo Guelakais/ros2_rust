05dba489d5e5825d7047d454eead3d723e4551ad
Guelakais <koroyeldiores@gmail.com>
2024-07-01 20:56:37 +0200

    fix to: ``` stack backtrace:    0: rust_begin_unwind              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/std/src/panicking.rs:645:5    1: core::panicking::panic_nounwind_fmt::runtime              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/panicking.rs:110:18    2: core::panicking::panic_nounwind_fmt              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/panicking.rs:123:9    3: core::panicking::panic_nounwind              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/panicking.rs:156:5    4: core::slice::raw::from_raw_parts::precondition_check              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/intrinsics.rs:2799:21    5: core::slice::raw::from_raw_parts              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/slice/raw.rs:98:9    6: rclrs::node::graph::convert_names_and_types              at ./src/node/graph.rs:425:9    7: rclrs::node::graph::<impl rclrs::node::Node>::get_names_and_types_by_node              at ./src/node/graph.rs:345:12    8: rclrs::node::graph::<impl rclrs::node::Node>::get_publisher_names_and_types_by_node              at ./src/node/graph.rs:86:9    9: rclrs::node::graph::tests::test_graph_empty              at ./src/node/graph.rs:493:32   10: rclrs::node::graph::tests::test_graph_empty::{{closure}}              at ./src/node/graph.rs:462:26   11: core::ops::function::FnOnce::call_once              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/ops/function.rs:250:5   12: core::ops::function::FnOnce::call_once              at /rustc/9b00956e56009bab2aa15d7bff10916599e3d6d6/library/core/src/ops/function.rs:250:5 note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace. thread caused non-unwinding panic. aborting. error: test failed, to rerun pass `--lib` ```

--- rclrs/src/node/graph.rs
+++ rclrs/src/node/graph.rs
@@ -420,6 +420,11 @@ fn convert_names_and_types(
 ) -> HashMap<String, Vec<String>> {
     let mut names_and_types: TopicNamesAndTypes = HashMap::new();
 
+    // Check if the names.size is valid
+    if rcl_names_and_types.names.size > isize::MAX as usize {
+        return Err("Invalid names.size value");
+    }
+    
     // SAFETY: Safe if the rcl_names_and_types arg has been initialized by the caller
     let name_slice = unsafe {
         slice::from_raw_parts(
