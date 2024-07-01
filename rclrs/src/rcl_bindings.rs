cb34066d978557e3a94dac875f8aa40895224692
GueLaKais <koryeldiores@gmail.com>
2024-07-01 22:32:47 +0200

    added macros again

--- rclrs/src/rcl_bindings.rs
+++ rclrs/src/rcl_bindings.rs
@@ -6,6 +6,8 @@
 // Added to avoid clippy complaining about u128 types not being FFI safe
 // Caused by https://github.com/ros2/ros2/issues/1374 in iron and newer
 // See https://github.com/ros2-rust/ros2_rust/issues/320
+#![allow(improper_ctypes)]
+#![allow(improper_ctypes_definitions)]
 #![allow(clippy::all)]
 #![allow(missing_docs)]
 
